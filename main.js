7d8e48a5a8b5191d80839894476f824da300c4b3
Akinori Sakakibara <gorion.1213@gmail.com>
2020-06-08 20:06:48 +0900

    second commit for ToDo-List

--- main.js
+++ main.js
@@ -1,5 +1,43 @@
-`use strict`
+(() => {
 
-{
+  const todos = [];
   
-}
+  const inputBox = document.getElementById('input-todo-box');
+  const addButton = document.getElementById('add-button');
+  const listContainer = document.getElementById('todo-list');
+  
+  addButton.addEventListener('click', (event) => {
+    
+    const todo = inputBox.value;
+    inputBox.value = '';
+    
+    console.log(todo);
+    if (todo) {
+      todos.push(todo);
+      showTodos();
+    }
+  });
+
+  const showTodos = () => {
+    while(listContainer.firstChild) {
+      listContainer.removeChild(listContainer.firstChild);
+    }
+    todos.forEach((todo, index) => {
+      const todoItem = document.createElement('tr');
+      const taskNumber = index + 1;
+
+      todoItem.textContent = `${taskNumber} : ${todo}`;
+      listContainer.appendChild(todoItem);
+
+      const conditionButton = document.createElement('button');
+      conditionButton.textContent = '作業中';
+      todoItem.appendChild(conditionButton);
+
+      const deleteButton = document.createElement('button');
+      deleteButton.textContent = '削除';
+      todoItem.appendChild(deleteButton);
+    });
+  };
+
+  
+})();
